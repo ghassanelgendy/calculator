5a29a285067731df772af339fd4afe9a513ab1c2
Ghassan <ghassanelgendyy@gmail.com>
2023-07-18 01:28:26 +0300

    Add Operation Function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
 const numbers = document.querySelectorAll(".number-button");
 const operators = document.querySelectorAll(".operator");
+
+let firstOperand = 0;
+let secondOperand = 0;
+let operator;
 let res = 0;
+
 numbers.forEach((number) => number.addEventListener("click", (e) => {}));
 operators.forEach((operator) => operator.addEventListener("click", (e) => {}));
+
+function operate(firstOperand, operator, secondOperand) {
+	switch (operator) {
+		case "*":
+			multiply(firstOperand, secondOperand);
+			break;
+		case "/":
+			divide(firstOperand, secondOperand);
+			break;
+		case "+":
+			add(firstOperand, secondOperand);
+			break;
+		case "-":
+			minus(firstOperand, secondOperand);
+	}
+}
+
+function multiply(firstOperand, secondOperand) {
+	res += firstOperand * secondOperand;
+}
+
+function divide(firstOperand, secondOperand) {
+	res += firstOperand * secondOperand;
+}
 
 function add(firstOperand, secondOperand) {
 	res += firstOperand * secondOperand;
 }
-function multiply(firstOperand, secondOperand) {
-	res += firstOperand * secondOperand;
-}
-function divide(firstOperand, secondOperand) {
-	res += firstOperand * secondOperand;
-}
+
 function minus(firstOperand, secondOperand) {
 	res += firstOperand - secondOperand;
 }
