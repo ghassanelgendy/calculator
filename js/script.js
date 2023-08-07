fcc22b590e65aa771586207b88de5dfab5d20549
Ghassan <ghassanelgendyy@gmail.com>
2023-08-07 04:37:18 +0300

    Publish

--- js/script.js
+++ js/script.js
@@ -3,74 +3,75 @@ const operators = document.querySelectorAll(".operator");
 const screen = document.getElementById("displaying-screen");
 const equal = document.getElementById("equals");
 const AC = document.getElementById("AC");
-AC.addEventListener("click", (e) => {
-	clearAll();
-	updateScreen("0");
-});
-let first;
-let second;
-let firstOperand = [];
-let equation = [];
-let secondOperand = [];
-let currentlyDisplaying;
-let operatorSign;
+
+let firstOperation = true;
+let first = 0;
+let second = 0;
 let res = 0;
+let firstOperand = [];
+let secondOperand = [];
+let currentlyDisplaying = "0";
+let operatorSign = null;
+let operatorClicked = false;
+let equation = [];
+
 screen.textContent = currentlyDisplaying;
+
 function updateScreen(text) {
 	if (text.length < 15) {
 		screen.textContent = text;
 	}
 }
+
 function clearAll() {
-	currentlyDisplaying = 0;
 	firstOperand = [];
 	secondOperand = [];
-	equation = [];
 	first = 0;
-	res = 0;
 	second = 0;
+	operatorSign = null;
+	operatorClicked = false;
+	equation = [];
+	res = 0;
 }
-numbers.forEach(
-	(
-		number //awel rakam input
-	) =>
-		number.addEventListener("click", (e) => {
+
+AC.addEventListener("click", (e) => {
+	clearAll();
+	screen.textContent = "0";
+});
+
+numbers.forEach((number) =>
+	number.addEventListener("click", (e) => {
+		if (!operatorClicked) {
 			firstOperand.push(number.textContent.replace(/\s/g, ""));
+			first = parseInt(firstOperand.join().replace(/,/g, ""));
 			equation.push(number.textContent.replace(/\s/g, ""));
 			updateScreen(equation.join().replace(/,/g, ""));
-		})
+		} else {
+			secondOperand.push(number.textContent.replace(/\s/g, ""));
+			second = parseInt(secondOperand.join().replace(/,/g, ""));
+			equation.push(number.textContent.replace(/\s/g, ""));
+			updateScreen(equation.join().replace(/,/g, ""));
+		}
+	})
 );
 operators.forEach((operator) =>
 	operator.addEventListener("click", (e) => {
 		operatorSign = operator.textContent;
 		first = parseInt(firstOperand.join().replace(/,/g, ""));
 		equation.push(operator.textContent.replace(/\s/g, ""));
-		console.log("wahed", first);
-
-		firstOperand = [];
-		secondOperand = [];
-
-		numbers.forEach(
-			(
-				number //tany rakam input
-			) =>
-				number.addEventListener("click", (e) => {
-					if (first) {
-						secondOperand.push(number.textContent.replace(/\s/g, ""));
-						second = parseInt(secondOperand.join().replace(/,/g, ""));
-						updateScreen(equation.join().replace(/,/g, ""));
-						console.log("HOPOOOOOOOO", secondOperand);
-						console.log("etnen", second);
-					}
-				})
-		);
+		updateScreen(equation.join().replace(/,/g, ""));
+		operatorClicked = true;
 	})
 );
+
 equal.addEventListener("click", (e) => {
 	console.log("me two ", second);
 	console.log("me first ", first);
 	console.log("me op ", operatorSign);
+	console.log("me res");
 	operate(first, operatorSign, second);
+	firstOperation = false;
+
 	clearAll();
 });
 function operate(first, operator, second) {
@@ -108,3 +109,29 @@ function add(x, y) {
 function minus(x, y) {
 	res += x - y;
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalButton"); // Add an ID to the button you want to use to open the pop-up
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+	modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
